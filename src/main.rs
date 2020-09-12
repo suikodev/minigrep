670f05166cb08e13ad3828566be70ac8e7c153e2
nacht42 <nacht42@riseup.net>
2020-09-13 00:40:50 +0800

    refactor function about parsing args

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,34 @@
 use std::env;
 use std::fs;
-fn main () {
+use std::process;
+use std::error::Error;
+fn main() {
     let args: Vec<String> = env::args().collect();
-    let (query, filename) = parse_config(&args);
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1)
+    });
+    run(config);
 }
 
-fn parse_config(args: &[String]) -> (&str, &str) {
-    let query = &args[1];
-    let filename = &args[2];
-
-    (query, filename)
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+    println!("{}", contents);
+    Ok(())
 }
 
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("args not enough");
+        }
+        let query = args[1].clone();
+        let filename = args[2].clone();
+        Ok(Config { query, filename })
+    }
+}
