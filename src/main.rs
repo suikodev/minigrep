a6beecc74066959485a7ab14810ee9a1982a2cb5
Nacht <nacht42@riseup.net>
2020-09-12 10:24:35 +0800

    parse args via function

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+use std::fs;
+fn main () {
+    let args: Vec<String> = env::args().collect();
+    let (query, filename) = parse_config(&args);
 }
+
+fn parse_config(args: &[String]) -> (&str, &str) {
+    let query = &args[1];
+    let filename = &args[2];
+
+    (query, filename)
+}
+
