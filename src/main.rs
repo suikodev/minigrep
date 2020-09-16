eda4c1f108f1eb2f7b90f15b622c1a41fe30162d
Nacht <nacht42@riseup.net>
2020-09-16 22:22:18 +0800

    handle error in main, extract functions to lib

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,16 @@
 use std::env;
-use std::fs;
 use std::process;
-use std::error::Error;
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    let config = Config::new(&args).unwrap_or_else(|err| {
+    let config = minigrep::Config::new(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {}", err);
         process::exit(1)
     });
-    run(config);
-}
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {}", e);
 
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.filename)?;
-    println!("{}", contents);
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    filename: String,
-}
-
-impl Config {
-    fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("args not enough");
-        }
-        let query = args[1].clone();
-        let filename = args[2].clone();
-        Ok(Config { query, filename })
+        process::exit(1);
     }
 }
+
